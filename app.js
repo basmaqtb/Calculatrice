6a2645b03460b9a709b808d4f3ad2dc0b823a77a
basmaqtb <qotbibasma8@gmail.com>
2024-03-14 15:00:27 +0000

    j'ai ajouter tous les fonction du js il me rest quelque details d'affichage

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
+
+window.alert("hello client");
+
 const display = document.getElementById("display");
+const display_input = document.getElementById("display_input");
 
 function appendToDisplay(input){
+    display_input.value += input;
     display.value += input;
 }
 
 function clearDisplay(){
     display.value = "";
+    display_input.value = ""
 }
 
 function calculate(){
     try{
         display.value = eval(display.value);
+
     }catch(error){
         display.value = "Error"
     }
 }
 
 function suppDisplay(){
-    display.value = display.value.toString().slice(0,-1);
+    display.value = display.value.slice(0,-1);
 }
 
 function parenthèseDisplay(input) {
+    display_input.value += input;
     display.value += input;
 }
+
+
 
