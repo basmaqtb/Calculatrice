6cd1a39f8943cbef25e0e9091515d24de6187b19
basmaqtb <qotbibasma8@gmail.com>
2024-03-15 15:51:25 +0000

    il me reste une nouvelle fonction dans jds que je vais l'ajout'é ce weekend

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
+var display = document.getElementById("display");
+var display_input = document.getElementById("display_input");
 
-window.alert("hello client");
-
-const display = document.getElementById("display");
-const display_input = document.getElementById("display_input");
 
 function appendToDisplay(input){
+    console.log(display_input.value);
     display_input.value += input;
     display.value += input;
+    
 }
 
 function clearDisplay(){
     display.value = "";
-    display_input.value = ""
+    display_input.value = "";
 }
 
 function calculate(){
@@ -19,18 +19,16 @@ function calculate(){
         display.value = eval(display.value);
 
     }catch(error){
-        display.value = "Error"
+        display.value = "Error";
     }
 }
 
 function suppDisplay(){
     display.value = display.value.slice(0,-1);
+    display_input.value = display_input.value.slice(0,-1);
 }
 
 function parenthèseDisplay(input) {
     display_input.value += input;
     display.value += input;
 }
-
-
-
